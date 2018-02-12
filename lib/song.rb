fef5105bc648fe8ab9676e3e4ee48a42d1f3b5d7
Alicia Yu <alicia.yu.16@gmail.com>
2018-02-12 20:55:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,37 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+  	c = self.new
+  	c.save
+  	c
+  end
+
+  def self.new_by_name(hash)
+    c = self.new
+	  c.name = hash[:name]
+	  c.artist_name = hash[:artist_name]
+
+  end
+
+  def self.find_or_create_by_name
+
+  end
+
+  def self.alphabetical
+
+  end
+
+  def self.new_from_filename
+
+  end
+
+  def self.create_from_filename
+
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
 end
