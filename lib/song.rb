0e1a7f5ad0c5ef3ce69c1bf2e297dd5b8ee79320
Alicia Yu <alicia.yu.16@gmail.com>
2018-02-12 20:58:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,59 +11,36 @@ class Song
   end
 
   def self.create
-    song = Song.new
-    song.save
-    song
+  	c = self.new
+  	c.save
+  	c
   end
 
-  def self.new_by_name(song_name)
-    song = self.new
-    song.name = song_name
-    song
+  def self.new_by_name(hash)
+    c = self.new
+	  c.name = hash[:name]
+	  c.artist_name = hash[:artist_name]
+
   end
 
-  def self.create_by_name(song_name)
-    song = self.create
-    song.name = song_name
-    song
-  end
+  def self.find_or_create_by_name
 
-  def self.find_by_name(song_name)
-    self.all.detect{|s| s.name == song_name}
-  end
-
-  def self.find_or_create_by_name(song_name)
-    self.find_by_name(song_name) || self.create_by_name(song_name)
   end
 
   def self.alphabetical
-    self.all.sort_by{|s| s.name}
+
   end
 
-  def self.new_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name = parts[0]
-    song_name = parts[1].gsub(".mp3", "")
+  def self.new_from_filename
 
-    song = self.new
-    song.name = song_name
-    song.artist_name = artist_name
-    song
   end
 
-  def self.create_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name = parts[0]
-    song_name = parts[1].gsub(".mp3", "")
+  def self.create_from_filename
 
-    song = self.create
-    song.name = song_name
-    song.artist_name = artist_name
-    song
   end
 
   def self.destroy_all
-    self.all.clear
+    @@all = []
   end
 
 end
